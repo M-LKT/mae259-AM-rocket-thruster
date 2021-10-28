b5aab8c56acd4626e0bf9aac996ba305b009f0dc
zz-tan <92891243+zz-tan@users.noreply.github.com>
2021-10-27 18:07:29 -0700

    Update mae252_rocket_engine_project.m
    Added calculation for the heat flux qw and code to print out the dimensions of the cooling channel.

--- mae252_rocket_engine_project.m
+++ mae252_rocket_engine_project.m
@@ -48,28 +48,25 @@ clc, clear
 
 %% Properties
 k = 320; % W/m^2-K Thermal conductivity of chamber wall
-Tmax = 500+273; % K, service temp of C18150
+Tmax = 480 + 273; % K, service temp of C18150 = 500C
 
 %% Parameters
-L = 0.01; % Wall thickness of combustion chamber
+L = 5*10^-3; % Wall thickness of combustion chamber, m
 asprt = 2; % Aspect Ratio
 
 TL_lox = 90; % K, LOX temperature
 TL_CH4 = 111; % K, CH4 temperature
 
 mdot = 0.66138; % kg/s
-mdot_lox = 0.50007; %kg/s
-Twa = 2500; % Addiabatic wall temperature
-qw = 5.5*10^6; % Heat flux at the throat
+mdot_lox = 0.50007; % kg/s
 
-
-Twa = 2500; % K, Addiabatic wall temperature
-qw = 3000; % K, Heat flux at the throat
+Twa = 2500; % K, Addiabatic wall temperature = Free Stream Stagnation Temp = Service Temp, approx
 
 hg = 2.6*10^3; % W/m^2-K Hot-Gas side heat transfer coefficient
 qr = 0; % Neglecting radiative heat transfer
 
 %% Calculations
+qw = hg*(Twa - Tmax); % Heat flux at the throat, W/m^2
 Twa_e = Twa + qr/hg;
 
 % Hydraulic Diameter for LOX Cooling Channels
@@ -92,3 +89,7 @@ height_lox = asprt*base_lox;
 
 base_CH4 = D_CH4*(1 + asprt)/(2*asprt);
 height_CH4 = asprt*base_CH4;
+
+%% Printing Dimensions for the Cooling Channel
+fprintf('CH4 Cooling Channel Dimension: Base = %2.4f mm, Height = %2.4f mm\n', base_CH4*10^3, height_CH4*10^3)
+fprintf('LOX Cooling Channel Dimension: Base = %2.4f mm, Height = %2.4f mm\n', base_lox*10^3, height_lox*10^3)
